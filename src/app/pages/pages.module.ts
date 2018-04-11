b03b50989caf89e0866bc3e123992818c6c5b33f
Mario Serrano <maseflores79@gmail.com>
2018-04-11 12:10:37 -0700

    Se crearon modulos para paginas y shared

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,23 @@
+import { SharedModule } from './../shared/shared.module';
+import { NgModule } from '@angular/core';
+
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { ProgressComponent } from './progress/progress.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagesComponent } from './pages.component';
+
+@NgModule({
+declarations: [
+    PagesComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component
+],
+exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component
+],
+imports: [ SharedModule ]
+})
+export class PagesModule {}

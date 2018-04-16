4a5890868e3b613f5f5b909f875bdd7d39dbb1c0
Mario Serrano <maseflores79@gmail.com>
2018-04-16 12:33:46 -0700

    Se creó servicio para controlar el SideBar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
+
 @NgModule(
-{    declarations: [BreadcrumbsComponent, HeaderComponent, NopagefoundComponent, SidebarComponent],
+{
+    imports: [RouterModule, CommonModule],
+    declarations: [BreadcrumbsComponent, HeaderComponent, NopagefoundComponent, SidebarComponent],
     exports : [BreadcrumbsComponent, HeaderComponent, NopagefoundComponent, SidebarComponent]
 })
 export class SharedModule { }
+/* OJO, para poder utilizarlo en los componentes involucrados en este módulo:
+    - RouterModule se necesita para RouterLink, RouterLinkActive,
+    - CommonModule para los ngIfs, Pipes, etc
+ */

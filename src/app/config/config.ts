db868fb82c2364097bc4cd3bfe1f408d4af2240b
Mario Serrano <maseflores79@gmail.com>
2018-05-23 22:10:08 -0700

    Agregada carpeta docs y adaptado a backend en Heroku

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,1 +1,2 @@
-export const URL_SERVICIOS = 'http://localhost:3000';
+/* export const URL_SERVICIOS = 'http://localhost:3000'; */
+export const URL_SERVICIOS = 'https://fathomless-retreat-12572.herokuapp.com';
